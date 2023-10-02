b5b3c197d8d2cc28b82936bdf4d8c2d2282a61eb
Ricardo Reyes <88251948+Ricardo-ReyesJr@users.noreply.github.com>
2023-10-01 20:19:58 -0500

    Project not complete need to finish and fix why it will not display.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'matrixb': '#060407',
+        'matrixr': '#841211',
+        'matrixg': '#9DBEB3',
+        'matrixlg': '#DCEEF0',
+        'deadpoolb': '#070C0F',
+        'deadpoolg': '#34424F',
+        'deadpoolw': '#CACAD2',
+        'deadpoolr': '#5F262F',
+        'customblack': '#040D12',
+        'customdarkgreen': '#123D3D',
+        'customhalfgreen': '#5C8374',
+        'customlightgreen': '#93B1A6'
+      }
+    },
+  },
+  plugins: [],
+}
+
